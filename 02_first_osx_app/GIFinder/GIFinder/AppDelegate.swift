23ea4874edfa16b4f2665dcbc3faa601d61581e0
Sam Davies <sam@razeware.com>
2015-07-17 11:52:56 +0100

    First commit of GIFinder

---
+++ 02_first_osx_app/GIFinder/GIFinder/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  GIFinder
+//
+//  Created by Sam Davies on 17/07/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(aNotification: NSNotification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(aNotification: NSNotification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ 02_first_osx_app/GIFinder/GIFinder/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GIFinder
+//
+//  Created by Sam Davies on 17/07/2015.
+//  Copyright © 2015 Razeware. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
