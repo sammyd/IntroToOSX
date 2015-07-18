6260a18963a0e783ad158ea0fca8cfa5333abcca
Sam Davies <sam@razeware.com>
2015-07-18 20:38:49 +0100

    Wired up the search field

--- 02_first_osx_app/GIFinder/GIFinder/ViewController.swift
+++ 02_first_osx_app/GIFinder/GIFinder/ViewController.swift
@@ -26,7 +26,7 @@ class ViewController: NSViewController {
   
   @IBOutlet weak var collectionView: NSCollectionView!
   
-  private var giphyItems : [GiphyItem]? {
+  var giphyItems : [GiphyItem]? {
     didSet {
       collectionView.reloadData()
     }
@@ -38,19 +38,6 @@ class ViewController: NSViewController {
     // Do any additional setup after loading the view.
     collectionView.dataSource = self
     collectionView.minItemSize = NSSize(width: 200, height: 200)
-    
-    searchGiphy("clever dog") {
-      result in
-      switch result {
-      case .Error(let error):
-        print(error.localizedDescription)
-      case .Result(let giphyItems):
-        print(giphyItems)
-        dispatch_async(dispatch_get_main_queue()) {
-          self.giphyItems = giphyItems
-        }
-      }
-    }
   }
 }
 
