f0295504a5ffc53c0aec46677a0762fea24b6237
Sam Davies <sam@razeware.com>
2015-11-21 07:33:17 +0700

    Create new sample project

---
+++ 02a_first_osx_app/prototyping/MagicEight/MagicEight/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  MagicEight
+//
+//  Created by Sam Davies on 21/11/2015.
+//  Copyright © 2015 Ray Wenderlich. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
